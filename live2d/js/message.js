b87b33606c6b5df1343e550ead611ff409154ee7
YaoRemu <917757853@qq.com>
2019-03-17 01:03:02 +0800

    Site updated: 2019-03-17 01:03:01

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -1,4 +1,4 @@
-var home_Path = document.location.protocol +'//' + window.document.location.hostname +':4000/';
+var home_Path = document.location.protocol +'//' + window.document.location.hostname +'/';
 
 var userAgent = window.navigator.userAgent.toLowerCase();
 console.log(userAgent);
